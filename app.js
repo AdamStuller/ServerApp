35d34f2938c66af2cc9240f63f86ff7a190cf8b4
Adam Stuller <adam.stuller@protonmail.com>
2018-12-23 18:44:26 +0100

    simple server complete

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req , res) => {
-    console.log(req.url  , req.headers ,  req.method);
+    //console.log(req.url  , req.headers ,  req.method);
 
-    res.setHeader('Content-Type' , 'text/html');
-    res.write('<html>');
-    res.write('<head><title>First Page</title></head>');
-    res.write('<body><h1>Welcome to my server! </h1></body>');
-    res.end();
-    
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>');
+        res.write('<head><title>First Page</title></head>');
+        res.write('<body> <form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<html>');
+   
+        res.end();
+        return ;
+    }
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) =>{
+            body.push(chunk);
+        })
+        return req.on('end', () => {
+            const message = Buffer.concat(body).toString().split('=')[1];
+            fs.writeFile('Message.txt' , message , (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location' , '/');
+                res.end();
+                return;
+            });
+        })   
+    }
+
 });
 
 server.listen(3000);
